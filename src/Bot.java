362c300fc1c38674e0f572936efeebe555991ac2
KrSergei <NemO-S@yandex.ru>
2018-04-15 19:42:30 +0300

    v1.1

--- src/Bot.java
+++ src/Bot.java
@@ -1,26 +1,78 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class Bot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update.getMessage().getFrom().getFirstName() + ": " + update.getMessage().getText());
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет, я робот");
+            if (message.getText().equals("/weather") || message.getText().equals("Weather"))
+                sendMsg(message, "Сейчас погода в Петербурге: " + "\nтемпература C\nвлажность %");
+            else
+                sendMsg(message, "Hello, " + message.getFrom().getFirstName());
+        }
+    }
 
-        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
-        sendMessage.setText("Hello" + update.getMessage().getFrom().getFirstName() + "\n" + update.getMessage().getText());
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add("Help");
+        keyboardFirstRow.add("Settings");
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add("Weather");
+//        keyboardSecondRow.add("To Do");
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
+
+
+
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
         try {
             sendMessage(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-
     }
+
+
 
     @Override
     public String getBotUsername() {
-        return null;
+        return "WeatherBot";
     }
 
     @Override
