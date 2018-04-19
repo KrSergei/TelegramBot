ad2139a0705149aa036f13fc2cf5e9bc9a402e78
KrSergei <NemO-S@yandex.ru>
2018-04-19 22:09:09 +0300

    v1.1

---
+++ src/JsonWeather.java
@@ -0,0 +1,84 @@
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class JsonWeather {
+    public static String parseUrl(URL url) {
+        if (url == null) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        // открываем соедиение к указанному URL
+        // помощью конструкции try-with-resources
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
+
+            String inputLine;
+            // построчно считываем результат в объект StringBuilder
+            while ((inputLine = in.readLine()) != null) {
+                stringBuilder.append(inputLine);
+                System.out.println(inputLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+    public static void parseCurrentWeatherJson(String resultJson) {
+        try {
+            // конвертируем строку с Json в JSONObject для дальнейшего его парсинга
+            JSONObject weatherJsonObject = (JSONObject) JSONValue.parseWithException(resultJson);
+
+            // получаем название города, для которого смотрим погоду
+            System.out.println("Название города: " + weatherJsonObject.get("name"));
+
+            // получаем массив элементов для поля weather
+            /* ... "weather": [
+            {
+                "id": 500,
+                    "main": "Rain",
+                    "description": "light rain",
+                    "icon": "10d"
+            }
+            ], ... */
+            JSONArray weatherArray = (JSONArray) weatherJsonObject.get("weather");
+            // достаем из массива первый элемент
+            JSONObject weatherData = (JSONObject) weatherArray.get(0);
+
+            // печатаем текущую погоду в консоль
+            System.out.println("Погода на данный момент: " + weatherData.get("main"));
+            // и описание к ней
+            System.out.println("Более детальное описание погоды: " + weatherData.get("description"));
+
+        } catch (org.json.JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String buildWeatherJson() {
+        // для простоты примера просто хардкодим нужные данные в методе
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name", "Лондон");
+        jsonObject.put("main", "Солнечно");
+        jsonObject.put("description", "Мороз трескучий, На небе ни единой тучи");
+
+        return jsonObject.toString();
+    }
+
+    // создаем объект URL из указанной в параметре строки
+    public static URL createUrl(String link) {
+        try {
+            return new URL(link);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import java.net.URL;
+
 public class Main {
     public static void main (String args[]){
         ApiContextInitializer.init();
@@ -14,4 +16,23 @@ public class Main {
         }
 
     }
+    public static final String WEATHER_URL =
+            "http://api.openweathermap.org/data/2.5/weather?q=London,uk" +
+                    "&units=metric&appid=241de9349721df959d8800c12ca4f1f3";
+
+        URL url = JsonWeather.createUrl(WEATHER_URL);
+
+        // загружаем Json в виде Java строки
+        String resultJson = JsonWeather.parseUrl(url);
+        System.out.println("Полученный JSON:\n" + resultJson);
+
+        // парсим полученный JSON и печатаем его на экран
+        JsonWeather.parseCurrentWeatherJson(resultJson);
+
+        // формируем новый JSON объект из нужных нам погодных данных
+        String json = JsonWeather.buildWeatherJson();
+        System.out.println("Созданный нами JSON:\n" + json);
+
+
+
 }
